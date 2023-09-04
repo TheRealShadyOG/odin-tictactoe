02a15f0cfc1e2e9f33884e2bb8934d4e44d87099
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-04 17:34:20 -0500

    Add modules and factory

---
+++ script.js
@@ -0,0 +1,18 @@
+// Store gameboard in an Object as an Array - module
+
+const gameBoard = (() => {
+    let gameBoard = ['', '', '', '', '', '', '', '', '']
+
+})();
+
+// Store players in Objects - factory
+
+const playerFactory = (name, player) => {
+    return {name, player};
+};
+
+// Store flow of game in Object - module
+
+const displayController = (() => {
+    
+})();
