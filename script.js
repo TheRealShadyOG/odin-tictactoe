ac81a2748797d2f950078733d0c7182ea704f72e
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-05 13:03:03 -0500

    Add functionality to click to place marker

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Store gameboard in an Object as an Array - module
 
 const gameBoard = (() => {
-    let gameBoard = ['X', 'X', 'O', 'O', 'O', 'X', 'X', 'O', 'X']
-    
+    let gameBoard = ['', '', '', '', '', '', '', '', '']
+
     return {gameBoard: gameBoard} ;
 })();
 
@@ -15,27 +15,45 @@ const playerFactory = (player) => {
         marker = 'O';
     }
 
-    return {marker};
+    return {player, marker};
 };
 
 // Store flow of game in Object - module
 
 const displayController = (() => {
-    let _grid = document.querySelectorAll('.box')
+    let _grid = document.querySelectorAll('.box');
+    let _player1 = playerFactory(1);
+    let _player2 = playerFactory(2);
+    let _currentPlayer = _player1;
 
-    for (let i = 0; i < _grid.length; i++) {
-        _grid[i].textContent = gameBoard.gameBoard[i];
+    function _displayGrid() {
+        for (let i = 0; i < _grid.length; i++) {
+            _grid[i].textContent = gameBoard.gameBoard[i];
+        }
     }
-    
+
+    _grid.forEach((e) => {
+        e.addEventListener('click', _updateGameBoard);
+    })
+
+    function _updateGameBoard(e) {
+        let _box = e.target.id
+        if (gameBoard.gameBoard[_box] === '') {
+            gameBoard.gameBoard[_box] = _currentPlayer.marker;
+            _displayGrid();
+            _playerSwap();
+        }
+    }
+
+    function _playerSwap() {
+        if (_currentPlayer === _player1) {
+            _currentPlayer = _player2;
+        } else {
+            _currentPlayer = _player1;
+        }
+    }
+
 })();
 
-
-// allow players to mark specific spot
-
-// tie player spot to dom
-// have eventlistener tied to gameBoard
-
-// cant click if occupied
-// if statement to do nothing if player clicks
 
 
