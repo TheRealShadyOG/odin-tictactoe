b70b83b56857b31c0736d07e7c25ba0c6bc24909
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-05 10:23:41 -0500

    Add code to displayController

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Store gameboard in an Object as an Array - module
 
 const gameBoard = (() => {
-    let gameBoard = ['', '', '', '', '', '', '', '', '']
-
+    let gameBoard = ['X', 'X', 'O', 'O', 'O', 'X', 'X', 'O', 'X']
+    
+    return {gameBoard: gameBoard} ;
 })();
 
 // Store players in Objects - factory
@@ -14,5 +15,10 @@ const playerFactory = (name, player) => {
 // Store flow of game in Object - module
 
 const displayController = (() => {
+    let _grid = document.querySelectorAll('.box')
+
+    for (let i = 0; i < _grid.length; i++) {
+        _grid[i].textContent = gameBoard.gameBoard[i];
+    }
     
 })();
